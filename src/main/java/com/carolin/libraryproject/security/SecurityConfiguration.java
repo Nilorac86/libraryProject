6393ea7d72dc5b8b0cfeb33385e4fbbf253bbd14
carolineriksson <carolin86@live.se>
2025-08-29 14:08:23 +0200

    Ändringar av filterchain

--- src/main/java/com/carolin/libraryproject/security/SecurityConfiguration.java
+++ src/main/java/com/carolin/libraryproject/security/SecurityConfiguration.java
@@ -3,14 +3,12 @@ package com.carolin.libraryproject.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,12 +20,23 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
+                // .requestMatchers för get kan vara public, post, put och delete behöver inloggning.
+                // Sedan ska roller tilldelas och autentisering tilldelas för de olika rollerna.
+
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.GET, "/books").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyRequest().hasRole("ADMIN")
                 )
-                .formLogin( form -> form.permitAll());
+
+                //.httpBasic(Customizer.withDefaults());
+                .formLogin(form -> form.permitAll())
+
+                .logout(logout -> logout.permitAll());
+
+
 
         return http.build();
 
@@ -37,9 +46,11 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+
     }
 
-}
+    }
+
 
 
 //                .authorizeHttpRequests(auth -> auth
