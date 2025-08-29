cb69854cd65323f5cb0850ba6609a9924730e0cd
carolineriksson <carolin86@live.se>
2025-08-29 14:02:50 +0200

    Lagt till exceptionhandler

---
+++ src/main/java/com/carolin/libraryproject/exceptionHandler/EmailAlreadyExcistException.java
@@ -0,0 +1,8 @@
+package com.carolin.libraryproject.exceptionHandler;
+
+public class EmailAlreadyExcistException extends RuntimeException {
+
+    public EmailAlreadyExcistException(String message) {
+        super();
+    }
+}
